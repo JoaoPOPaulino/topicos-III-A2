053573603359163412204e1eea8636a09758f4ea
João Pedro <joaopoliveirapaulino@gmail.com>
2025-11-27 18:00:04 -0300

    Corr

--- A2/obj/Debug/net9.0/A2.AssemblyInfo.cs
+++ A2/obj/Debug/net9.0/A2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacd44d43785aa2aca8ae67c9f68fcf33c8429ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c401c16c92c828f797fad77c5c63baf894bcf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("A2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
